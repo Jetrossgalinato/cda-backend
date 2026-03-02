b745d2c6f469a6ac39ad1372c537b8cc848e7be8
Jetross <jetrossneri07@gmail.com>
2026-03-02 22:50:25 +0800

    feat: add dynamic API

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
-def read_root():
-    return {"message": "Hello, FastAPI + uv!"}
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/about")
+async def about():
+    return {"message": "This is the about API!"}
+
+@app.get("/users/{user_id}")
+async def get_user(user_id: int):
+    return {"user": user_id}
