19103d31830a7a49ad6ea85b3e5d155a34e77c51
Jetross <jetrossneri07@gmail.com>
2026-03-03 17:23:26 +0800

    feat: add prefix (users.py)

--- api/users.py
+++ api/users.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter
 
-router = APIRouter()
+router = APIRouter(prefix="/users")
 
-@router.get("/users/search")
+@router.get("/search")
 async def search_user(query: int):
     return {"query": query}
 
-@router.get("/users/{user_id}")
+@router.get("/{user_id}")
 async def get_user(user_id: int):
     return {"user": user_id}
